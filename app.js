712beabc64ed467e45716ce6d1aa281cb62ef473
Luan Nascimento <164895664+nascmentoluan@users.noreply.github.com>
2025-01-31 17:05:55 -0300

    Update app.js: adicionando comentários ao código

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
+//Array para ser usado durante a execução das funções
 let nomeDosAmigos = [];
 
+//Função para receber nomes digitados e armazená-los no arrray
 function adicionarAmigo(){
-
+    
+    //adiciona nomes a var nomeDoAmigo
     let nomeDoAmigo = document.querySelector("#amigo").value;
 
+    //verifica se campo enviado não está vazio e se já está no array. Caso contrário,
+    //adiciona valor da var nomeDoAmigo ao array
     if (nomeDoAmigo == "" || nomeDoAmigo == " "){
         alert("erro: Por favor, insira um nome");
     } else if (nomeDosAmigos.includes(nomeDoAmigo)){
@@ -16,10 +21,16 @@ function adicionarAmigo(){
     }
 }
 
+//cria uma lista com nomes armazenados no array
 function criarListaDeAmigos(){
+
+    //"conecta" pelo id elemento ul a var listaAmigos
     let listaAmigos = document.querySelector("#listaAmigos");
-    listaAmigos.innerHTML = "";
     
+    //atualiza a lista, limpa a cada execução
+    listaAmigos.innerHTML = "";
+
+    //percorre o array, criar novos elementos li para cada nome adicionado ao array, pela var item e adiciona ao elemento ul
     for (let i = 0; i < nomeDosAmigos.length; i++){
       let item = document.createElement("li");
         item.textContent = nomeDosAmigos[i];
@@ -30,14 +41,21 @@ function criarListaDeAmigos(){
 
 
 function sortearAmigo(){
+    //verifica pelo tamanho da lista se ela não está vazia. caso contrário, 
+    
     if (nomeDosAmigos.length == 0){
         alert("erro: Por favor, adicione um ou mais amigos para serem sorteados");
     } else {
+
+        //  funções matemáticas para sortear um número e deixá-lo inteiro
         let numeroSorteado = Math.random() * nomeDosAmigos.length;
         let deixandoNumeroInteiro = Math.floor(numeroSorteado);
+        //atribui numero sorteado presente no array a var amigoSorteado pelos índices do array
         let amigoSorteado = nomeDosAmigos[deixandoNumeroInteiro];
+        //pelo id, conecta var resultado ao elemento ul
         let resultado = document.getElementById("resultado");
         resultado.innerHTML = "";
+        //adiciona e cria elementos li com valores na var nomeSorteado
         let nomeSorteado = document.createElement("li");
         nomeSorteado.textContent = amigoSorteado;
         resultado.appendChild(nomeSorteado);
